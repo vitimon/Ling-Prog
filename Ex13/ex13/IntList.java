aad7de5a5fb6ddb5b45bf569eb64a22d28d9beb6
Your Name <leonardongc@gmail.com>
2022-10-25 20:27:23 -0300

    improved identation

--- Ex13/ex13/IntList.java
+++ Ex13/ex13/IntList.java
@@ -1,37 +1,33 @@
 package ex13;
 
-/**
- * An IntList is a list of ints.
- */
 public class IntList {
- private ConsCell start; // list head, or null
- /**
- * Construct a new IntList given its first ConsCell.
- * @param s the first ConsCell in the list, or null
- */
- public IntList(ConsCell s) {
- start = s;
- }
- /**
- * Cons the given element h onto us and return the
- * resulting IntList.
- * @param h the head int for the new list
- * @return the IntList with head h, and us as tail
- */
- public IntList cons (int h) {
- return new IntList(new ConsCell(h,start));
- }
- /**
- * Get our length.
- * @return our int length
- */
- public int length() {
- int len = 0;
- ConsCell cell = start;
- while (cell != null) { // while not at end of list
- len++;
- cell = cell.getTail();
- }
- return len;
- }
+    private ConsCell start;
+    public IntList(ConsCell s) {
+        start = s;
+    }
+
+    public IntList cons (int h) {
+        return new IntList(new ConsCell(h,start));
+    }
+
+    public int length() {
+        int len = 0;
+        ConsCell cell = start;
+        while (cell != null) { // while not at end of list
+            len++;
+            cell = cell.getTail();
+        }
+        return len;
+    }
+/*
+ public IntList append(IntList y){
+    int thisHead = this.ConsCell.getHead();
+    ConsCell thisTail = this.ConsCell.getTail();
+    int yHead = y.ConsCell.getHead();
+    ConsCell yTail = y.ConsCell.getTail();
+
+
+
+    return IntList(thisHead);
+ }*/
 }
